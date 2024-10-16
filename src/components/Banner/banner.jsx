b002d4d31165e1bb9a4af65e1b76bdb15ded970b
Angerome22 <angeline.jerome12@gmail.com>
2024-10-16 17:20:45 +0200

    responsive et opacité de l'image sur la galerie

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -3,7 +3,7 @@ export default function Banner({ picture, text, dark }) {
         <div className={`banner ${dark ? "banner-dark" : ""}`}>
             <img src={picture} alt="bannière" />
             <div>
-                <h1> {text && <h1>{text}</h1>}</h1>
+                <div> {text && <h1>{text}</h1>}</div>
             </div>
         </div>        
         
